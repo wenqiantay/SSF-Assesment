fa03ebdcbf16b2914045028ea74ef00ed799a797
wenqiantay <wenqianhello@gmail.com>
2024-12-13 09:09:33 +0800

    initial commit

---
+++ src/main/java/vttp/batch5/ssf/noticeboard/controllers/NoticeController.java
@@ -0,0 +1,7 @@
+package vttp.batch5.ssf.noticeboard.controllers;
+
+// Use this class to write your request handlers
+
+public class NoticeController {
+
+}
---
+++ src/main/java/vttp/batch5/ssf/noticeboard/services/NoticeService.java
@@ -0,0 +1,11 @@
+package vttp.batch5.ssf.noticeboard.services;
+
+public class NoticeService {
+
+	// TODO: Task 3
+	// You can change the signature of this method by adding any number of parameters
+	// and return any type
+	public void postToNoticeServer() {
+
+	}
+}
